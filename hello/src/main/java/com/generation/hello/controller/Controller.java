741aa320ad02f954590bd6b2284be13b11fc1596
erikakuo <erikakuo@hotmail.com>
2022-02-24 00:11:16 -0300

    Aprendendo a fazer o Alô Mundão

---
+++ hello/src/main/java/com/generation/hello/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.generation.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+
+
+public class Controller {
+ 
+	@GetMapping
+	
+	public String hello( ) {
+		return "Bem vindo ao Guarujá";
+		
+	}
+}
